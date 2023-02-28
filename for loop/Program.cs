a0e3fd8fb6068f9868c9114c0350caf793318220
Suyash0707 <suyashb175@gmail.com>
2023-02-28 18:37:09 +0530

    Add project files.

---
+++ for loop/Program.cs
@@ -0,0 +1,57 @@
+﻿
+
+using System.Net.NetworkInformation;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Schema;
+
+public class program
+{
+    public static void Main()
+    {
+        pattern1();
+        pattern2();
+        pattern3();
+        pattern4();
+ 
+        static void pattern1()
+        {
+            Console.WriteLine("Display 1-10 numbers");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        }
+
+        static void pattern2()
+        {
+            Console.WriteLine("Display 10-1 numbers");
+            for (int i = 10; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        }
+
+        static void pattern3()
+        {
+            Console.WriteLine("Display 0-10 numbers by +2");
+            for (int i = 0; i <=10; i = i + 2) 
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        }
+
+        static void pattern4()
+        {
+            Console.WriteLine("Display 10-0 numbers by -2");
+            for (int i = 10; i >= 0; i = i - 2)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        }
+    }
+}
